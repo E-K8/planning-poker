c5406665abb9bd62c03bde0c7dd58e9aca7920df
E-K8 <ekatplat@gmail.com>
2024-12-05 22:38:34 +0000

    feat: style card buttons: gaps, size, wrapping

--- ppoker/components/CardSelector.tsx
+++ ppoker/components/CardSelector.tsx
@@ -1,9 +1,13 @@
 const CardSelector = ({ onVote }: { onVote: (value: number) => void }) => {
   const values = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
   return (
-    <div>
+    <div className='card-container'>
       {values.map((value) => (
-        <button className='green' key={value} onClick={() => onVote(value)}>
+        <button
+          className='card-button'
+          key={value}
+          onClick={() => onVote(value)}
+        >
           {value}
         </button>
       ))}
