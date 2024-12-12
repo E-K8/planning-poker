c6666fa5cfb8ca2dd2a976a2a8b3374c6fca7893
E-K8 <ekatplat@gmail.com>
2024-12-12 21:19:50 +0000

    feat: style AverageDisplay in a box

--- ppoker/components/AverageDisplay.tsx
+++ ppoker/components/AverageDisplay.tsx
@@ -5,7 +5,12 @@ const AverageDisplay = ({ users }: { users: User[] }) => {
   const voteCount = users.filter((user) => user.vote !== null).length;
   const average = voteCount ? totalVotes / voteCount : 0;
 
-  return <div className='red'>Average: {average.toFixed(2)}</div>;
+  return (
+    <div className='average-container'>
+      <h2 className='average-title'>Average:</h2>
+      <p className='average-value'>{average.toFixed(2)}</p>
+    </div>
+  );
 };
 
 export default AverageDisplay;
